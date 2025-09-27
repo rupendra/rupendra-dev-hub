653fa918f9fec24bc1ec73296f4bb18d196fb613
rupendra <rupendragonnade1@gmail.com>
2025-09-27 21:27:32 +0530

    fix

--- src/app/layouts/header/header.ts
+++ src/app/layouts/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MATERIAL_IMPORTS } from '../../shared/material.imports';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [...MATERIAL_IMPORTS],
+  imports: [...MATERIAL_IMPORTS, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
